57c64b57c2a063208fc4bf23b61326b615034d8c
Nayem Hoaasin <nayemhosen79@gmail.com>
2024-04-01 13:06:08 +0300

    use.py

--- robot_code/configs/use.py
+++ robot_code/configs/use.py
@@ -1,3 +1,13 @@
+import time
+
 def mapping(x,min_val,max_val,aim_min,aim_max):
     x = aim_min + abs((x - min_val) / (max_val- min_val) * (aim_max-aim_min))
     return x
+
+def soft_reset():
+    from robot_code.utils.pin import Pin
+    soft_reset_pin = Pin("D16")
+    soft_reset_pin.low()
+    time.sleep(0.01)
+    soft_reset_pin.high()
+    time.sleep(0.01)
