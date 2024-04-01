ab690c66b580e76d69ba4a32f347069eaec430c0
Nayem Hoaasin <nayemhosen79@gmail.com>
2024-04-01 12:59:03 +0300

    pwm.py

--- robot_code/utils/pwm.py
+++ robot_code/utils/pwm.py
@@ -1,5 +1,12 @@
 import smbus, math
-from .i2c import I2C
+import os
+import sys
+# Add the parent directory of robot_code to sys.path
+script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
+parent_dir = os.path.join(script_dir, '..', '..')  # Navigate two levels up
+sys.path.append(os.path.abspath(parent_dir))
+
+from robot_code.utils.i2c import I2C
 
 class PWM(I2C):
     REG_CHN = 0x20
